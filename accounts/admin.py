064f48d5b9a18a21d3d31b81b703664ad6100e0b
whale <you@example.com>
2021-08-31 14:56:19 +0100

    admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -16,13 +16,13 @@ class AcountAdmin(UserAdmin):
     list_filter = ()
     fieldsets = ()
 
-# class UserProfileAdmin(admin.ModelAdmin):
-#     def thumbnail(self, object):
-#         return format_html('<img src="{}" width="30" style="boader-radius:50%;">'.format(object.profile_picture.url))
-#     thumbnail.short_description = 'Profile Picture'
-#     list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+class UserProfileAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="30" style="boader-radius:50%;">'.format(object.profile_picture.url))
+    thumbnail.short_description = 'Profile Picture'
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
 
 
 
-# admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(Account, AcountAdmin)
